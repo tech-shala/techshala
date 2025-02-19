085262b9481984b74f6e9be53909d5ef46ac4c18
Aryan shinde <shindearyan179@gmail.com>
2025-02-19 20:35:06 +0530

    Added a Favicon.ico and update the Title of the website

--- app/layout.js
+++ app/layout.js
@@ -55,7 +55,7 @@ export default function RootLayout({ children }) {
       <head>
         <meta name="author" content="Laralink" />
         <link rel="icon" href="/images/favicon.ico" sizes="any" />
-        <title>Arino Creative Agency Next JS Template</title>
+        <title>Techshala | VP | Technical Committee</title>
       </head>
       <body className={`${openSans.variable} ${poppins.variable}`}>
         <Toaster position="bottom-right" />
